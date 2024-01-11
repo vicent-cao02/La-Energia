2f5eb54f53e4b09403dd5250f9b4e1a33236f050
vicent-cao02 <139924476+vicent-cao02@users.noreply.github.com>
2024-01-11 04:41:34 +0100

    Upgrade

---
+++ DataProdut/DataProdut.py
@@ -0,0 +1,12 @@
+import os, folium, streamlit as st, pandas as pd
+
+
+
+
+
+db=pd.read_json(os.path.join(os.path.dirname(__file__), '..', 'base de datos.json'))
+db=db.transpose() # Transponer el dataframe
+db = db.drop('Info', axis=1)
+
+with st.container():
+    st.title("La Energía")
